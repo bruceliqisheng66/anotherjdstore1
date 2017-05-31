55d292a40c5bd46f655a2ad2bc8ca94e632e9634
liqisheng <bruce.liqisheng66@gmail.com>
2017-05-31 16:24:45 +0800

    add order controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,8 @@
 class Order < ApplicationRecord
+  belongs_to :user
+
+  validates :billing_name, presence: true
+  validates :billing_addresss, presence: true
+  validates :shipping_name, presence: true
+  validates :shipping_addresss, presence: true
 end
